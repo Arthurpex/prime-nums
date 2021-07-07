06ea4f212d672fd0d8a50b3702f945d3024ebf4c
Arthur Pereira <arthur.pereira@khomp.com>
2021-07-07 14:13:42 -0300

    user input

--- main.c
+++ main.c
@@ -13,11 +13,30 @@ static bool IsPrime(int number)
 
 
 
+
+
+
+
 int main(int argc, char *argv[])
 {
+
+    int max_number;
+    int threads_num;
+
+    if(argc<3){
+        printf("\nNot enough arguments to start program");
+        printf("\nUsage: ./prime <max_number> <number_of_threads>\n");
+        return 0;
+    };
+
+    // max_number = argv[1];
+    // threads_num = argv[2];
+    printf("Max number %s\n", argv[1]);
+    printf("Number of threads %s\n", argv[2]);
+
 	int loop;
-    int numbers[1001];
-    for(int i = 0; i <= 1000; i++){
+    int numbers[11];
+    for(int i = 0; i <= 10; i++){
     numbers[i] = i;
 }
 	//calculate length of the array
@@ -29,7 +48,7 @@ int main(int argc, char *argv[])
 	{
 		// printf("%3d - %s\n",numbers[loop],(isPrime(numbers[loop])?"Prime":"Not Prime"));
         if(IsPrime(numbers[loop]))
-            printf("%d\n", numbers[loop]);
+             printf("%d\n", numbers[loop]);
 	}
 
 	printf("\n");
